7071db5f72806bc3124a8aadea5261e13faaaa14
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-08-28 09:20:55 -0700

    Initial commit--adding skeleton for frontend and backend code

---
+++ code/backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const server = express;
+
+const PORT = 3000;
+
+// TODO: SERVER GET
+
+// TODO: SERVER POST
+
+// TODO: SERVER PUT
+
+// TODO: SERVER DELETE
+
+server.listen(PORT);
