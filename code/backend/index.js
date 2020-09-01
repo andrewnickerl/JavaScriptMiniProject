aa0118039ba9710e262db8dfd82e8934792a9db7
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-09-01 10:25:35 -0700

    Adding gitignore, a Mongoose models file with the user.js model, and changing index.js to use the Mongoose client instead of MongoDbClient

--- code/backend/index.js
+++ code/backend/index.js
@@ -1,14 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const User = require("./models/user");
 const server = express();
+const dbConnStr =
+  "mongodb+srv://dbUser:Passw0rd123@cluster0.uxrjs.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
-const PORT = 3000;
+mongoose.connect(dbConnStr, { useNewUrlParser: true });
 
-// TODO: SERVER GET
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Database connected");
+});
+db.on("error", (err) => {
+  console.error("Connection error: ", err);
+});
 
-// TODO: SERVER POST
+function saveUser(user) {
+  let u = new User(user);
+  return u.save();
+}
 
-// TODO: SERVER PUT
+// GET ALL
+server.get("/", (req, res) => {
+    res.send(await User.find());
+});
 
-// TODO: SERVER DELETE
-
-server.listen(PORT);
+// GET USER WITH SPECIFIED USERNAME
+server.get("/", (req, res) => {
+    let username = req.body.username;
+    res.send(await User.find(username))
+})
